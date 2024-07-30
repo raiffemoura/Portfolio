7562b1d150f28d1c2a9a17ba153062f090274298
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2024-07-30 15:31:32 -0300

    style: add styles and animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Menu from "./_components/menu";
 
 export default function Home() {
   return (
-    <div className="flex h-[100vh] w-[100vw] flex-col justify-between bg-[url('./_assets/fundo-preto.jpg')] bg-cover bg-center">
+    <div className="flex h-[100vh] w-[100vw] flex-col justify-between bg-[url('./_assets/colorfull.jpg')] bg-cover bg-center">
       <HomeCard />
       <div className="mb-4 flex items-center justify-center">
         <Menu selected="home" />
