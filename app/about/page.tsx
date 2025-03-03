94f5ca20f44efc50339a5c6c2f6deb79ba6466ba
Raiffe <1993.raiffe@gmail.com>
2025-03-03 12:16:12 -0300

    updated assets

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Menu from "../_components/menu";
 
 const AboutPage = () => {
   return (
-    <div className="flex h-[100vh] w-[100vw] flex-col justify-between overflow-auto bg-[url('./_assets/colorfull.jpg')] bg-cover bg-center max-md:h-fit max-md:pb-20">
+    <div className="flex h-[100vh] w-[100vw] flex-col justify-between overflow-hidden bg-[url('./_assets/colorfull.jpg')] bg-cover bg-center max-md:h-fit max-md:pb-20">
       <AboutCard />
       <div className="mb-4 flex items-center justify-center">
         <Menu selected="about" />
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Menu from "./_components/menu";
 
 export default function Home() {
   return (
-    <div className="flex h-[100vh] w-[100vw] flex-col justify-between overflow-auto bg-[url('./_assets/colorfull.jpg')] bg-cover bg-center">
+    <div className="flex h-[100vh] w-[100vw] flex-col justify-between overflow-hidden bg-[url('./_assets/colorfull.jpg')] bg-cover bg-center">
       <HomeCard />
       <div className="mb-4 flex items-center justify-center">
         <Menu selected="home" />
