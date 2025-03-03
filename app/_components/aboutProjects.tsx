6f5ef2e3a09aaae2fc6bcf4bc594553154629302
Raiffe <1993.raiffe@gmail.com>
2025-03-03 11:32:22 -0300

    updated projects

--- app/_components/aboutProjects.tsx
+++ app/_components/aboutProjects.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { Card } from "./ui/card";
-import ecommerce from "../_assets/ecommerce.jpg";
 import portfolios from "../_assets/portfol-ios.png";
 import ifoodfy from "../_assets/ifoodfy.jpg";
 import pbbarbers from "../_assets/pbbarbers.jpg";
+import institucional from "../_assets/institucional.jpg";
+import landingpage from "../_assets/landingpage.jpg";
+import ecomerce from "../_assets/ecommerce3.jpg";
+
 import Link from "next/link";
 
 const AboutProjects = () => {
@@ -127,36 +130,102 @@ const AboutProjects = () => {
             </div>
           </Card>
         </Link>
-        <Link href="/projects">
+
+        <Link href="https://catedralcomunhao.framer.website/">
           <Card className="item flex h-fit w-[25rem] flex-col items-center justify-center gap-3 rounded-3xl border-none bg-black bg-opacity-30 p-4 text-white hover:bg-opacity-50 max-md:w-[21rem]">
             <Image
-              src={ecommerce}
-              alt="ecommerce"
+              src={institucional}
+              alt="Catedral Anglicana"
               className="h-[100%] w-[100%] rounded-xl"
             />
             <div className="flex w-full flex-col justify-start text-xl">
-              <h1 className="text-xl font-bold">E-commerce WP</h1>
+              <h1 className="text-xl font-bold">Catedral Anglicana</h1>
               <p className="text-sm text-gray-300">
-                Um projeto de e-commerce em construção, desenvolvido com
-                WordPress para demonstrar habilidades na criação de lojas online
-                intuitivas e responsivas.
+                Site institucional da Igreja Catedral Anglicana, projetado para
+                oferecer uma navegação intuitiva e um design acolhedor,
+                refletindo a identidade e missão da comunidade cristã.
               </p>
               <br />
               <p className="text-sm text-gray-300">
-                Este e-commerce inclui funcionalidades como gerenciamento de
-                produtos, carrinho de compras, sistema de checkout seguro e
-                integração com plataformas de pagamento.
+                O site inclui páginas de eventos, álbuns de fotos, materiais
+                para download e informações de contato, garantindo
+                acessibilidade e engajamento para os membros e visitantes.
               </p>
-              <div className="mt-3 flex gap-2">
+              <div className="mt-3 flex gap-1">
                 <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
-                  <p className="text-xs">WordPress</p>
+                  <p className="text-xs">Framer</p>
                 </Card>
                 <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
-                  <p className="text-xs">WooCommerce</p>
+                  <p className="text-xs">UX/UI Design</p>
                 </Card>
-
+              </div>
+            </div>
+          </Card>
+        </Link>
+        <Link href="https://veraselopesbpcloas.framer.website/">
+          <Card className="item flex h-fit w-[25rem] flex-col items-center justify-center gap-3 rounded-3xl border-none bg-black bg-opacity-30 p-4 text-white hover:bg-opacity-50 max-md:w-[21rem]">
+            <Image
+              src={landingpage}
+              alt="Veras & Lopes Advocacia"
+              className="h-[100%] w-[100%] rounded-xl"
+            />
+            <div className="flex w-full flex-col justify-start text-xl">
+              <h1 className="text-xl font-bold">Veras & Lopes Advocacia</h1>
+              <p className="text-sm text-gray-300">
+                Landing page desenvolvida para um escritório de advocacia,
+                focada na captação de leads para clientes interessados no
+                benefício BPC LOAS.
+              </p>
+              <br />
+              <p className="text-sm text-gray-300">
+                O site é altamente persuasivo, com estrutura otimizada para
+                conversão, CTA&apos;s estratégicos e formulário integrado para
+                contato direto com os advogados. Foi desenvolvido para ter alta
+                performance em impulsionamentos pagos, garantindo melhor
+                captação de leads qualificados.
+              </p>
+              <div className="mt-3 flex gap-1">
                 <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
-                  <p className="text-xs">MySQL</p>
+                  <p className="text-xs">Framer</p>
+                </Card>
+                <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
+                  <p className="text-xs">Landing Page</p>
+                </Card>
+                <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
+                  <p className="text-xs">Otimização de Conversão</p>
+                </Card>
+              </div>
+            </div>
+          </Card>
+        </Link>
+        <Link href="https://benifalcao.lojavirtualnuvem.com.br/">
+          <Card className="item flex h-fit w-[25rem] flex-col items-center justify-center gap-3 rounded-3xl border-none bg-black bg-opacity-30 p-4 text-white hover:bg-opacity-50 max-md:w-[21rem]">
+            <Image
+              src={ecomerce}
+              alt="Beni Falcão"
+              className="h-[100%] w-[100%] rounded-xl"
+            />
+            <div className="flex w-full flex-col justify-start text-xl">
+              <h1 className="text-xl font-bold">Beni Falcão</h1>
+              <p className="text-sm text-gray-300">
+                E-commerce especializado em enxovais minimalistas, oferecendo
+                produtos de alta qualidade com design sofisticado e funcional.
+              </p>
+              <br />
+              <p className="text-sm text-gray-300">
+                A plataforma conta com uma experiência de compra fluida,
+                integração com meios de pagamento e logística eficiente,
+                garantindo uma jornada agradável para os clientes.
+              </p>
+              <div className="mt-3 flex gap-1">
+                <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
+                  <p className="text-xs">Nuvemshop</p>
+                </Card>
+                <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
+                  <p className="text-xs">E-commerce</p>
+                </Card>
+                <Card className="flex w-fit items-center justify-center gap-2 rounded-sm border-none bg-[#1d1d1d] bg-opacity-40 px-3 py-1 text-gray-300 hover:bg-[#474747]">
+                  <p className="text-xs">UX/UI Design</p>
                 </Card>
               </div>
             </div>
